021eb48bf74bbb8ec1afd36b0838e46d649a7f47
1ndz <mxmortada92@gmail.com>
2025-11-09 01:22:41 +0300

    Deploy to Vercel

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,9 @@
 const nextConfig = {
-    reactStrictMode: false,
+    reactStrictMode: true,
+    swcMinify: true,
+    compress: true,
+    poweredByHeader: false,
+    output: 'standalone'
 };
 
 export default nextConfig;
