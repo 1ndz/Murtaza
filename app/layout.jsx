34b6b35e84855ff6b4c9301d1710a52492593214
1ndz <mxmortada92@gmail.com>
2025-11-10 16:01:48 +0300

    Deploy to Vercel

--- app/layout.jsx
+++ app/layout.jsx
@@ -37,6 +37,12 @@ export default function RootLayout({ children }) {
 
     return (
         <html lang="en">
+            <head>
+                <meta
+                    name="viewport"
+                    content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
+                />
+            </head>
             <body>
                 <div className="box">
                     <canvas id="fx"></canvas>
